4edb5fbd2b8ad3d860b4618c5d1cc6108c328f37
Armand Grillet <armand.grillet@gmail.com>
2014-05-17 17:47:08 +0200

    Window.onload removed.

--- src/js/preview.js
+++ src/js/preview.js
@@ -12,36 +12,31 @@ var innerPage;
 var previewTitle;
 var previewContent;
 
-window.onload = function() {
-	/*
-    * Shortcuts.
-    */
-    storyTextarea = document.getElementById("StoryComment");
-    innerPage = document.getElementsByClassName("InnerPage")[0];
+/*
+* Code.
+*/
+storyTextarea = document.getElementById("StoryComment");
+innerPage = document.getElementsByClassName("InnerPage")[0];
 
-    /*
-    * Functions.
-    */
-    /* Title */
-	previewTitle = document.createElement("div");
-	previewTitle.innerHTML = "<h1>Preview:</h1>";
-	previewTitle.style["padding-top"] = "40px";
-	innerPage.appendChild(previewTitle);
+/* Title */
+previewTitle = document.createElement("div");
+previewTitle.innerHTML = "<h1>Preview:</h1>";
+previewTitle.style["padding-top"] = "40px";
+innerPage.appendChild(previewTitle);
 
-	/* Post */
-	previewContent = document.createElement("div");
-	previewContent.className = "StoryBody";
-	innerPage.appendChild(previewContent);
-	previewContent.setAttribute('style', "font: 400 14px/24px \"Whitney SSm A\",\"Whitney SSm B\",\"Georgia\",\"Times New Roman\",Serif;");
+/* Post */
+previewContent = document.createElement("div");
+previewContent.className = "StoryBody";
+innerPage.appendChild(previewContent);
+previewContent.setAttribute('style', "font: 400 14px/24px \"Whitney SSm A\",\"Whitney SSm B\",\"Georgia\",\"Times New Roman\",Serif;");
 
-	/* Translation */
-	if (previewContent.innerHTML != "")
-		contentChanged();
-	storyTextarea.onkeyup = contentChanged;
-	storyTextarea.onpaste = contentChanged;
-}
+/* Translation */
+if (previewContent.innerHTML != "")
+	contentChanged();
+storyTextarea.onkeyup = contentChanged;
+storyTextarea.onpaste = contentChanged;
 
-/* Displays the post with its new content. */
+/* Displays the post with the textarea is updated. */
 function contentChanged () { 
 	marked(storyTextarea.value, function (err, content) {  
     	previewContent.innerHTML = content; 
